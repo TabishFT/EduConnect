b203a9157879815081f2ddc71c8f76e903b0ca7e
MdTabish24 <fiza8983khan@gmail.com>
2025-06-26 18:35:32 +0530

    latest update by MdTabish24

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends, HTTPException, status, Request, Response, UploadFile, File, fORM
+from fastapi import FastAPI, Depends, HTTPException, status, Request, Response, UploadFile, File, Form
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from fastapi.responses import JSONResponse
