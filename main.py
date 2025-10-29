d63142e27c4cd23f4fbc1909d5e7f2c1a55c6a06
MdTabish24 <fiza8983khan@gmail.com>
2025-10-29 23:32:48 +0530

    login page ki bbaari

--- main.py
+++ main.py
@@ -188,6 +188,9 @@ def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
 
 def get_password_hash(password):
+    # Bcrypt has 72 byte limit
+    if len(password.encode('utf-8')) > 72:
+        password = password[:72]
     return pwd_context.hash(password)
 
 def get_user(email: str):
